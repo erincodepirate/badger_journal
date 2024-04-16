482d56c8bcc63744d50bfc0c380b448605c814a4
Erin Clark <erincodepirate@gmail.com>
2024-04-15 19:09:41 -0500

    Add entry model

--- badger_journal_backend/api/models.py
+++ badger_journal_backend/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Entry(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    date = models.DateTimeField("date created")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.title)
