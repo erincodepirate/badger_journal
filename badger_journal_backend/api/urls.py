4942d824269e01c1a3c00569ab7d393415461b8e
Erin Clark <erincodepirate@gmail.com>
2024-04-15 19:24:30 -0500

    Add methods for viewing entries

--- badger_journal_backend/api/urls.py
+++ badger_journal_backend/api/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import entry_list, entry_detail
 
 urlpatterns = [
+    path('entries/', entry_list, name='entry_list'),
+    path('entries/<int:pk>', entry_detail, name='entry_detail')
 ]
--- badger_journal_backend/api/views.py
+++ badger_journal_backend/api/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.core.exceptions import ObjectDoesNotExist
+
+from .models import Entry
 
 # Create your views here.
+def entry_list(request):
+    """
+    Return all entries
+    """
+    entries = Entry.objects.all()
+
+    data = {'entries': list(entries.values())}
+
+    return JsonResponse(data)
+
+def entry_detail(request, pk):
+    """
+    Return a single entry
+    """ 
+    try:
+        entry = Entry.objects.get(pk = pk)
+    except ObjectDoesNotExist:
+        return JsonResponse({
+            'status_code': 404,
+            'error': f'Entry with id {pk} not found.'
+        })
+    else:
+        data = {
+            'id': entry.id,
+            'title': entry.title,
+            'text': entry.text,
+            'date': str(entry.date),
+            'user': entry.user.username
+        }
+        
+    return JsonResponse(data)
