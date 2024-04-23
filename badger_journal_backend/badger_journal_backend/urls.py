787bf199e4ba18ce75f5f1c441bcce3416127d56
Erin Clark <erincodepirate@gmail.com>
2024-04-23 11:20:15 -0500

    Reorganize urls.py

--- badger_journal_backend/badger_journal_backend/urls.py
+++ badger_journal_backend/badger_journal_backend/urls.py
@@ -24,10 +24,8 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls')),
     path("auth/", include("rest_framework.urls")),
-
-    # jwt
-    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/', include('api.urls')),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
